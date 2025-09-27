dd8c2c6b37897d8f5920a5c5020f51577d4f25c8
Kyle Johnson <gkjohns@gmail.com>
2025-09-27 15:30:15 -0400

    add vercel analytics

--- nuxt-app/app/app.vue
+++ nuxt-app/app/app.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { Analytics } from '@vercel/analytics/nuxt'
+
 useHead({
   meta: [
     { name: 'viewport', content: 'width=device-width, initial-scale=1' }
@@ -27,4 +29,5 @@ useSeoMeta({
   <UApp>
     <NuxtPage />
   </UApp>
+  <Analytics />
 </template>
